837af6ea5da4aff2c490d4e5c76b1f6d21574c3d
Antoniiocg7 <antoniocanizares8820@gmail.com>
2023-05-27 20:49:03 +0200

    Bug paginador solucionado

--- view/header_footer/header.php
+++ view/header_footer/header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src='/models/pageModel.js'></script>
+    <script src="../../models/pageModel.js"></script>
     <title>
         <?php
 
