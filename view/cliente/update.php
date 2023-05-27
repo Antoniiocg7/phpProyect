19a12d0981d0f16c7e83f9b05abaf9f9df903a6f
Antoniiocg7 <antoniocanizares8820@gmail.com>
2023-05-27 02:15:04 +0200

    Implementada Vista Update

---
+++ view/cliente/update.php
@@ -0,0 +1,8 @@
+<?php
+    require_once "../../controllers/clienteController.php";
+    $clienteController = new ClienteController();
+    $clienteController->update(
+        $_POST["dni"],$_POST["correo"],$_POST["contrasena"],$_POST["nombre"],
+        $_POST["apellido_1"],$_POST["apellido_2"],$_POST["telefono"],$_POST["direccion"]
+    );
+?>
