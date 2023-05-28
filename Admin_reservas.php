e8d1550662010c26b0c4d5f710af4c7184f42d53
sergiomanresa <sergiojmb8@gmail.com>
2023-05-28 16:32:14 +0200

    agregando paginador funcional y limitador de datos en la tabla

--- Admin_reservas.php
+++ Admin_reservas.php
@@ -13,8 +13,29 @@ if ($conn->connect_error) {
     die("Error al conectar a la base de datos: " . $conn->connect_error);
 }
 
-// Consultar las reservas de la tabla "reserva"
-$sql = "SELECT * FROM reserva";
+// Obtener el número de registros por página
+$limit = isset($_GET['limit']) ? $_GET['limit'] : 10;
+
+// Obtener el número total de registros
+$sql_total = "SELECT COUNT(*) as total FROM reserva";
+$result_total = $conn->query($sql_total);
+$row_total = $result_total->fetch_assoc();
+$total_records = $row_total['total'];
+
+// Calcular el número total de páginas
+$total_pages = ceil($total_records / $limit);
+
+// Obtener la página actual
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+// Asegurarse de que la página actual esté dentro del rango válido
+$page = max(1, min($page, $total_pages));
+
+// Calcular el offset para la consulta SQL
+$offset = ($page - 1) * $limit;
+
+// Consultar las reservas paginadas
+$sql = "SELECT * FROM reserva LIMIT $limit OFFSET $offset";
 $result = $conn->query($sql);
 ?>
 
@@ -43,11 +64,6 @@ $result = $conn->query($sql);
         </div>
 
         <div class="reservas">
-            <div class="search-bar">
-                <input type="text" id="search-input" placeholder="Buscar reservas...">
-                <button id="search-button">Buscar</button>
-            </div>
-            
             <div class="table-wrapper">
                 <table>
                     <tr>
@@ -76,30 +92,61 @@ $result = $conn->query($sql);
 
                 <div id="paginator">
                     <?php
-                    $limit = 10; // Número de registros por página
-                    $total_records = $result->num_rows; // Total de registros
-                    $total_pages = ceil($total_records / $limit); // Total de páginas
-
-                    if (!isset($_GET['page'])) {
-                        $page = 1;
+                    // Botón retroceder a la primera página
+                    if ($page > 1) {
+                        echo "<button><a href='?page=1&limit=$limit'>&lt;&lt;</a></button>";
                     } else {
-                        $page = $_GET['page'];
+                        echo "<button disabled>&lt;&lt;</button>";
                     }
 
-                    $start = ($page - 1) * $limit; // Registro de inicio para cada página
+                    // Botón retroceder una página
+                    if ($page > 1) {
+                        $prev_page = $page - 1;
+                        echo "<button><a href='?page=$prev_page&limit=$limit'>&lt;</a></button>";
+                    } else {
+                        echo "<button disabled>&lt;</button>";
+                    }
 
+                    // Botones de páginas
                     for ($i = 1; $i <= $total_pages; $i++) {
                         if ($i == $page) {
-                            echo "<button class='active'><a href='?page=" . $i . "'>" . $i . "</a></button>";
+                            echo "<button class='active'><a href='?page=$i&limit=$limit'>$i</a></button>";
                         } else {
-                            echo "<button><a href='?page=" . $i . "'>" . $i . "</a></button>";
+                            echo "<button><a href='?page=$i&limit=$limit'>$i</a></button>";
                         }
                     }
+
+                    // Botón avanzar una página
+                    if ($page < $total_pages) {
+                        $next_page = $page + 1;
+                        echo "<button><a href='?page=$next_page&limit=$limit'>&gt;</a></button>";
+                    } else {
+                        echo "<button disabled>&gt;</button>";
+                    }
+
+                    // Botón avanzar a la última página
+                    if ($page < $total_pages) {
+                        echo "<button><a href='?page=$total_pages&limit=$limit'>&gt;&gt;</a></button>";
+                    } else {
+                        echo "<button disabled>&gt;&gt;</button>";
+                    }
                     ?>
+                </div>
+
+                <div class="limit-selector">
+                    <form method="GET" action="">
+                        <label for="limit">Mostrar:</label>
+                        <select name="limit" id="limit">
+                            <option value="2" <?php echo $limit == 2 ? 'selected' : ''; ?>>2</option>
+                            <option value="20" <?php echo $limit == 4 ? 'selected' : ''; ?>>4</option>
+                            <option value="50" <?php echo $limit == 8 ? 'selected' : ''; ?>>8</option>
+                            <option value="100" <?php echo $limit == 10 ? 'selected' : ''; ?>>10</option>
+                        </select>
+                        <input type="submit" value="Actualizar">
+                    </form>
                 </div>
             </div>
         </div>
     </div>
-    <script src="script.js"></script>
 </body>
 </html>
